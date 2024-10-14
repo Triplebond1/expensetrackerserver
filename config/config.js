6de7b4244fe4eaff761cab21854dd9e785f98755
Triplebond <olayinkaadebisi05@gmail.com>
2024-10-14 01:07:26 +0100

    removed exposed config parameters

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 config = {
   development: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_NAME || 'expensedb',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
   jwtSecret: process.env.JWT_SECRET_KEY,
